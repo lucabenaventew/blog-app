29a99afb70d14eae556821f36ac87d497b6cebc0
lucabenaventew <cacachuuuluca@gmail.com>
2022-12-01 19:57:03 -0500

    Fix Linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,13 @@
 class Comment < ApplicationRecord
-    after_save :update_comments_counter
-    belongs_to :user
-    belongs_to :post
-  
-    validates :text, presence: true, length: { maximum: 100 }
-  
-    private
-  
-    def update_comments_counter
-      post.update(comments_counter: post.comments.count)
-    end
+  after_save :update_comments_counter
+  belongs_to :user
+  belongs_to :post
+
+  validates :text, presence: true, length: { maximum: 100 }
+
+  private
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,21 @@
 class Post < ApplicationRecord
-    after_save :update_posts_counter
-    has_many :comments
-    has_many :likes
-    belongs_to :user
-  
-    validates :title, presence: true, length: { maximum: 250 }
-    validates :text, presence: true, length: { maximum: 100 }
-    validates :likes_counter, :comments_counter, numericality: { only_integer: true },
-                                                 comparison: { greater_than_or_equal_to: 0 }
-  
-    def last_five_posts
-      comments.last(5)
-    end
-  
-    private
-  
-    def update_posts_counter
-      user.update(posts_counter: user.posts.count)
-    end
+  after_save :update_posts_counter
+  has_many :comments
+  has_many :likes
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true, length: { maximum: 100 }
+  validates :likes_counter, :comments_counter, numericality: { only_integer: true },
+                                               comparison: { greater_than_or_equal_to: 0 }
+
+  def last_five_posts
+    comments.last(5)
   end
+
+  private
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
+end
