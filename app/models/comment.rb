c50b35df02a0aca71f257a621556697b5ff0be8f
lucabenaventew <cacachuuuluca@gmail.com>
2022-12-01 19:55:51 -0500

    Add tests and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,13 @@
 class Comment < ApplicationRecord
-  after_save :update_comments_counter
-  belongs_to :user
-  belongs_to :post
-
-  private
-
-  def update_comments_counter
-    post.update(comments_counter: post.comments.length)
+    after_save :update_comments_counter
+    belongs_to :user
+    belongs_to :post
+  
+    validates :text, presence: true, length: { maximum: 100 }
+  
+    private
+  
+    def update_comments_counter
+      post.update(comments_counter: post.comments.count)
+    end
   end
-end
