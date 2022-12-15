41f7a2fec3fdfdc66503f0889afc0a6533e860ba
lucabenaventew <cacachuuuluca@gmail.com>
2022-12-15 14:37:39 -0500

    Add devise

--- db/migrate/20221018194607_create_users.rb
+++ db/migrate/20221018194607_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
-    def change
-      create_table :users do |t|
-        t.string :name
-        t.text :photo
-        t.text :bio
-        t.integer :posts_counter
-  
-        t.timestamps
-      end
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.text :photo, default: 'https://www.kindpng.com/picc/m/22-223965_no-profile-picture-icon-circle-member-icon-png.png'
+      t.text :bio
+      t.integer :posts_counter
+
+      t.timestamps
     end
   end
+end
